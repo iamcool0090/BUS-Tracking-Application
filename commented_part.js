2ad623ee202045919ffca0f7ff2cc7ca93cdd4f3
keyboardClicker <71261134+iamcool0090@users.noreply.github.com>
2024-02-25 01:03:51 +0530

    Delete commented_part.js

--- commented_part.js
+++
@@ -1,9 +0,0 @@
-
-// Create a pool of database connections
-// const pool = new pg.Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_DATABASE,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-// });
